a77c07e1148973fa4c2a1c93aec67b4ee07527ed
CloudLoveU <764280101@qq.com>
2017-09-05 17:56:56 +0800

    tj3

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -6,6 +6,9 @@ var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
+var VuxLoader = require('vux-loader');
+baseWebpackConfig=VuxLoader.merge(baseWebpackConfig, {plugins: ['vux-ui']})
+
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
   baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,61 @@
 <template>
   <div id="app">
-    <view-box></view-box>
+    <view-box>
+      <x-header
+      title="客有家租房"
+      :left-options="{showBack:false}"
+      slot="header">
+
+      </x-header>
+
+      <search></search>
+      <tabbar slot="bottom">
+        <tabbar-item>
+          <img src="./assets/icon_nav_button.png" alt="" slot="icon">
+          <span slot="label">所有房源</span>
+        </tabbar-item>
+        <tabbar-item>
+          <img src="./assets/icon_nav_cell.png" alt="" slot="icon">
+          <span slot="label">可租房源</span>
+        </tabbar-item>
+      </tabbar>
+    </view-box>
     <!-- <router-view></router-view> -->
   </div>
 </template>
 
 <script>
-import {ViewBox} from 'vux'
+import {ViewBox, XHeader, Tabbar, TabbarItem} from 'vux'
+import search from './components/search'
 export default {
   name: 'app',
   components: {
-    ViewBox
+    ViewBox,
+    XHeader,
+    Tabbar,
+    TabbarItem,
+    search
   }
 }
 </script>
 
 <style>
+/*@import '~vux/src/styles/reset.less';*/
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
+}
+#app{
+  height:100%;
+}
+html,body{
+  margin:0;
+  padding:0;
+  width:100%;
+  height:100%;
 }
 </style>
